74a57d4dca89f310a189eea48d7bd65ff3fbd56b
Cesar Augusto Silva Santos Sousa <cesar.sousa@provider-es.local>
2019-07-14 15:51:40 -0300

    wip

--- public/js/app.js
+++ public/js/app.js
@@ -1813,9 +1813,9 @@ __webpack_require__.r(__webpack_exports__);
       var _this = this;
 
       Echo.join('chat').joining(function (user) {
-        axios.put('/user/' + user.uuid + '/online?api_token=' + user.api_token, {});
+        axios.put('/user/' + user.uuid + '/online', {});
       }).leaving(function (user) {
-        axios.put('/user/' + user.uuid + '/offline?api_token=' + user.api_token, {});
+        axios.put('/user/' + user.uuid + '/offline', {});
       }).listen('UserOnline', function (e) {
         _this.friend = e.user;
       }).listen('UserOffline', function (e) {
