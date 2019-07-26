9b07ccfbfc1c2065ffb17ecad0f26ce2e4818d17
Cesar Augusto Silva Santos Sousa <cesar.sousa@provider-es.local>
2019-07-26 14:17:47 -0300

    wip

--- app/Notifications/FinishedTicket.php
+++ app/Notifications/FinishedTicket.php
@@ -32,7 +32,7 @@ class FinishedTicket extends Notification
      */
     public function via($notifiable)
     {
-        return ['database'];
+        return ['database', 'mail'];
     }
 
     /**
--- app/Notifications/NewMessage.php
+++ app/Notifications/NewMessage.php
@@ -32,7 +32,7 @@ class NewMessage extends Notification
      */
     public function via($notifiable)
     {
-        return ['database'];
+        return ['database', 'mail'];
     }
 
     /**
