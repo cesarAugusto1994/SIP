6489611fb1f442488c213174c31396f4876692c3
Cesar Augusto Silva Santos Sousa <cesar.sousa@provider-es.local>
2019-07-30 15:23:33 -0300

    wip

--- app/Http/Controllers/DeliveryOrderController.php
+++ app/Http/Controllers/DeliveryOrderController.php
@@ -325,21 +325,21 @@ class DeliveryOrderController extends Controller
     {
         $data = $request->request->all();
 
-        if($request->has('delivered_by')) {
+        if(!$request->has('delivered_by')) {
             notify()->flash('Erro de Envio', 'error', [
               'text' => 'Nenhum entregador foi informado.',
             ]);
             return back();
         }
 
-        if($request->has('address_id')) {
+        if(!$request->has('address_id')) {
             notify()->flash('Erro de Envio', 'error', [
               'text' => 'Nenhum endereço foi informado.',
             ]);
             return back();
         }
 
-        if($request->has('documents')) {
+        if(!$request->has('documents')) {
             notify()->flash('Erro de Envio', 'error', [
               'text' => 'Nenhum documento foi informado.',
             ]);
