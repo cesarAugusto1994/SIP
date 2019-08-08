478633b32ce099066f5da3a767a00104bfa6358f
Cesar Augusto Silva Santos Sousa <cesar.sousa@provider-es.local>
2019-08-08 13:42:33 -0300

    WIP

--- resources/views/pdf/tags.blade.php
+++ resources/views/pdf/tags.blade.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name') }} | Etiquetas</title>
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link href="http://cdn.jsdelivr.net/npm/inspinia@2.6.5/dist/inspinia.min.css"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="{{ asset('adminty\css\style.css') }}"/>
 
     <style>
 
