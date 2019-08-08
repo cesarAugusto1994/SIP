e5f2cfd7f3e975c06659076ca4dd77a5d014ef82
Cesar Augusto Silva Santos Sousa <cesar.sousa@provider-es.local>
2019-08-08 14:03:02 -0300

    WIP

--- app/Models/DeliveryOrder/Log.php
+++ app/Models/DeliveryOrder/Log.php
@@ -4,18 +4,17 @@ namespace App\Models\DeliveryOrder;
 
 use Illuminate\Database\Eloquent\Model;
 use Emadadly\LaravelUuid\Uuids;
-use Spatie\Activitylog\Traits\LogsActivity;
 
 class Log extends Model
 {
     use Uuids;
-    use LogsActivity;
+    //use LogsActivity;
 
     protected $table = 'delivery_order_logs';
 
     protected $fillable = ['status_id', 'delivery_order_id', 'user_id', 'message'];
 
-    protected static $logAttributes = ['status_id', 'delivery_order_id', 'user_id', 'message'];
+    //protected static $logAttributes = ['status_id', 'delivery_order_id', 'user_id', 'message'];
 
     public function user()
     {
