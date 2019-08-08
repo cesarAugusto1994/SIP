8d57a94dfa89870945664d556e1d4b069e0722e4
Cesar Augusto Silva Santos Sousa <cesar.sousa@provider-es.local>
2019-08-08 13:48:48 -0300

    WIP

--- resources/views/pdf/tags.blade.php
+++ resources/views/pdf/tags.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <link rel='shortcut icon' type='image/x-icon' href="{{asset('admin/img/RedukLogo/favicon.ico')}}" />
+    <link rel='shortcut icon' type='image/x-icon' href="{{ asset('images\favicon.ico') }}" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name') }} | Etiquetas</title>
