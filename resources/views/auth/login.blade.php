fca548aea269ec0eebb382dc6ccc45ccfd400eb6
Cesar Augusto Silva Santos Sousa <cesar.sousa@provider-es.local>
2019-10-07 14:47:48 -0300

    wip

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
 <form class="md-float-material form-material formValidation" action="{{ route('login') }}" method="POST">
     @csrf
     <div class="text-center m-b-30">
-        <img src="{{ asset('images\logo-provider.png') }}"  alt="SIP - Provider">
+        <img src="{{ asset('images\logo-provider.png') }}" style="width:40%"  alt="SIP - Provider">
     </div>
     <div class="auth-box card">
         <div class="card-block">
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -9,7 +9,7 @@
     <input type="hidden" name="token" value="{{ $token }}">
 
     <div class="text-center m-b-30">
-        <img src="{{ asset('images\logo-provider.png') }}" alt="SIP - Provider">
+        <img src="{{ asset('images\logo-provider.png') }}" style="width:40%"  alt="SIP - Provider">
     </div>
     <div class="auth-box card">
         <div class="card-block">
